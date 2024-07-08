bc19f20bdce0e4979144c47ef9f5b8e650f1aebf
Isaac Mullo <isaacmulloaimg@gmail.com>
2024-07-08 18:45:16 -0500

    Functional login added with context

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import {Routes, Route } from 'react-router-dom';
+import {Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css'
 import StockManage from './pages/StockManage/StockManage'
 import SalesHistory from './pages/SalesHistory/SalesHistory';
 import Sales from './pages/Sales/Sales';
 import Login from './pages/Login/Login';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Login/>}/>
-        <Route path={"/stock"} element={<StockManage/>}/>
-        <Route path={"/history"} element={<SalesHistory/>}/>
-        <Route path={"/sales"} element={<Sales/>}/>
+        <Route path="/" element={<Login />} />
+        <Route element={<ProtectedRoute roles={['admin']} />}>
+          <Route path="/stock" element={<StockManage />} />
+          <Route path="/history" element={<SalesHistory />} />
+        </Route>
+        <Route element={<ProtectedRoute roles={['admin', 'employee']} />}>
+          <Route path="/sales" element={<Sales />} />
+        </Route>
       </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './contexts/AuthContext/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <AuthProvider>
+          <App />
+      </AuthProvider>
   </React.StrictMode>,
 )
